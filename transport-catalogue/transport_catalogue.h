73ef1fe4ee0c3b1e7e6ec04d70956002de5daf3c
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-04-04 02:44:57 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,50 @@
-// место для вашего кода
+#pragma once
+
+#include <algorithm>
+#include <deque>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+
+#include "geo.h"
+
+namespace catalogue {
+struct Stop {
+    std::string name;
+    geo::Coordinates coordinates;
+};
+
+struct Bus {
+    std::string number;
+    std::vector<const Stop*> route;
+};
+
+struct BusInfo {
+    int stops_count;
+    int unique_stops_count;
+    double route_length;
+};
+
+class TransportCatalogue {
+    public:
+        void AddStop(const Stop* stop);                                                       //добавить остановку в базу
+        const Stop* FindStop(const std::string_view stop) const;                              //поиск остановки по имени 
+        void AddBus(const Bus* bus);                                                          //добавить маршрут в базу
+        const Bus* FindBus(const std::string_view bus) const;                                 //поиск маршрута по имени
+        const BusInfo GetBusInfo(const std::string_view bus) const;                           //получить информацию о маршруте
+        void BusesForStop(const std::string_view stop);                                       //добавить автобусы проходящие через остановку
+        const std::unordered_set<const Bus*> FindBusesForStop(const Stop* stop) const;        //поиск автобусов проходящих через остановку 
+        const std::set<std::string_view> GetStopInfo(const std::string_view stop_name) const; //получить информацию об остановке
+        int UniqueStopsCount(const Bus* bus) const;                                           //подсчет уникальных остановок
+        double CalculateRouteLength(const Bus* bus) const;                                    //подсчет длины маршрута
+    private:
+        std::deque<Stop> stops_;                                                             //остановки
+        std::unordered_map<std::string_view, const Bus*> busname_to_bus_;                    //индекс остановок(хеш - таблица)
+        std::deque<Bus> buses_;                                                              //маршруты
+        std::unordered_map<std::string_view, const Stop*> stopname_to_stop_;                 //индекс маршрутов(хеш - таблица)
+        std::unordered_map<const Stop*, std::unordered_set<const Bus*>> busname_to_stop_;    //автобусы проходящие через остановку
+};
+}
