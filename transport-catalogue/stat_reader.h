f6cdc0d3c60dd6c34ce265891b8ea13cd27ea56b
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-04-04 02:44:32 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,14 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+#include <iostream>
+#include <iomanip>
+
+#include "transport_catalogue.h"
+
+namespace detail {
+namespace outstat {
+void ParseAndPrintStat(catalogue::TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output);
+}
+}
