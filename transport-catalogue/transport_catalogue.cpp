b86bfd1ab3ef3c718399a762892cf4970faf8c34
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-04-04 18:28:28 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,12 +1,12 @@
 #include "transport_catalogue.h"
 
-void catalogue::TransportCatalogue::AddStop(const catalogue::Stop* stop){
-    stops_.push_back(*stop);
+void catalogue::TransportCatalogue::AddStop(const Stop& stop){
+    stops_.push_back(stop);
     stopname_to_stop_.insert({std::move(stops_.back().name), &stops_.back()});
 }
 
-void catalogue::TransportCatalogue::AddBus(const catalogue::Bus* bus){
-    buses_.push_back(*bus);
+void catalogue::TransportCatalogue::AddBus(const Bus& bus){
+    buses_.push_back(bus);
     busname_to_bus_.insert({std::move(buses_.back().number), &buses_.back()});
 }
 
@@ -24,46 +24,45 @@ const catalogue::Bus* catalogue::TransportCatalogue::FindBus(const std::string_v
     return nullptr;
 }
 
-const std::unordered_set<const catalogue::Bus*> catalogue::TransportCatalogue::FindBusesForStop(const catalogue::Stop* stop) const {
-    if (busname_to_stop_.count(stop)) {
-        return busname_to_stop_.at(stop);
+const std::unordered_set<const catalogue::Bus*> catalogue::TransportCatalogue::FindBusesForStop(const std::string_view stop_name) const {
+    const Stop* stop_ptr = FindStop(stop_name);    
+    if (busname_to_stop_.count(stop_ptr)) {
+        return busname_to_stop_.at(stop_ptr);
     }
     return {};
-}
-
-int catalogue::TransportCatalogue::UniqueStopsCount(const catalogue::Bus* bus) const{
-    std::unordered_set<const Stop*> unique_stops;
-    const auto& route = FindBus(bus->number)->route;
-    for (const auto& stop : route) {
-        unique_stops.insert(stop);    
-    }
-    return unique_stops.size();
-}
-
-double catalogue::TransportCatalogue::CalculateRouteLength(const catalogue::Bus* bus) const {
-    double route_length = 0.0;
-    const auto& route = FindBus(bus->number)->route;
-    for (int i = 1; i < route.size(); ++i) {
-        const catalogue::Stop* current_stop = route[i - 1];
-        const catalogue::Stop* next_stop = route[i];
-        route_length += ComputeDistance(current_stop->coordinates, next_stop->coordinates);
-    }
-    return route_length;
 }
 
 const catalogue::BusInfo catalogue::TransportCatalogue::GetBusInfo(const std::string_view bus_number) const {
     catalogue::BusInfo bus_info{};
     const catalogue::Bus* bus = FindBus(bus_number);
     if (bus) {
-        bus_info.stops_count = bus->route.size();
-        bus_info.unique_stops_count = UniqueStopsCount(bus);
-        bus_info.route_length = CalculateRouteLength(bus);
+        {
+            bus_info.stops_count = bus->route.size();
+        }
+        {
+            std::unordered_set<const Stop*> unique_stops;
+            const auto& route = FindBus(bus->number)->route;
+            for (const auto& stop : route) {
+                unique_stops.insert(stop);    
+            }
+            bus_info.unique_stops_count = unique_stops.size();
+        }
+        {
+            double route_length = 0.0;
+            const auto& route = FindBus(bus->number)->route;
+            for (int i = 1; i < route.size(); ++i) {
+                const catalogue::Stop* current_stop = route[i - 1];
+                const catalogue::Stop* next_stop = route[i];
+                route_length += ComputeDistance(current_stop->coordinates, next_stop->coordinates);
+            }
+            bus_info.route_length = route_length;           
+        }
     }
     return bus_info;
 }
 
-void catalogue::TransportCatalogue::BusesForStop(const std::string_view stop_name) {
-    const Stop* stop_ptr = FindStop(stop_name);
+void catalogue::TransportCatalogue::AddBusesForStop(const Stop& stop) {
+    const Stop* stop_ptr = FindStop(stop.name);
     for (const Bus& bus : buses_) {
         const auto& route = FindBus(bus.number)->route;            
         for (const auto& stop : route) {
@@ -78,7 +77,7 @@ const std::set<std::string_view> catalogue::TransportCatalogue::GetStopInfo(cons
     std::set<std::string_view> buses;  
     const Stop* stop_ptr = FindStop(stop_name);
     if (stop_ptr) {
-        for (const auto& bus : FindBusesForStop(stop_ptr)) {
+        for (const auto& bus : FindBusesForStop(stop_name)) {
             if (bus) {
                 buses.insert(std::move(bus->number));  
             }
