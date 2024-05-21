82234ae16c98b95374bb04603530109faaa53783
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-05-21 20:02:57 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -32,28 +32,52 @@ struct BusInfo {
 class TransportCatalogue {
     public:
     struct StopPairHasher {
-        std::size_t operator()(const std::pair<const Stop*, const Stop*>& p) const {                   //хэшер для пары указателей на остановки 
+        
+        //хэшер для пары указателей на остановки         
+        std::size_t operator()(const std::pair<const Stop*, const Stop*>& p) const {                   
         std::size_t hash1 = std::hash<const void*>{}(p.first);
         std::size_t hash2 = std::hash<const void*>{}(p.second);
-        return hash1 + hash2 * 37;                                                                     // Комбинируем хеши для обоих указателей
+            
+        //комбинируем хеши для обоих указателей            
+        return hash1 + hash2 * 37;                                                                     
     }
 };    
-        void AddStop(const Stop& stop);                                                                //добавить остановку в базу
-        const Stop* FindStop(const std::string_view stop) const;                                       //поиск остановки по имени 
-        void AddBus(const Bus& bus);                                                                   //добавить маршрут в базу
-        const Bus* FindBus(const std::string_view bus) const;                                          //поиск маршрута по имени
-        const BusInfo GetBusInfo(const std::string_view bus) const;                                    //получить информацию о маршруте
-        void AddBusesForStop(const Stop& stop);                                                        //добавить автобусы проходящие через остановку
-        const std::unordered_set<const Bus*> FindBusesForStop(const std::string_view stop_name) const; //поиск автобусов проходящих через остановку 
-        const std::set<std::string_view> GetStopInfo(const std::string_view stop_name) const;          //получить информацию об остановке
-        void SetDistance(const Stop* from, const Stop* to, const int distance);                        //задать дистанцию между остановками
-        int GetDistance(const Stop* from, const Stop* to) const;                                       //получить дистанцию между остановками
+        void AddStop(const Stop& stop);
+        const Stop* FindStop(const std::string_view stop) const;            
+        void AddBus(const Bus& bus);                                        
+        const Bus* FindBus(const std::string_view bus) const;               
+    
+        //получить информацию о маршруте    
+        const BusInfo GetBusInfo(const std::string_view bus) const;         
+    
+        //добавить автобусы проходящие через остановку
+        void AddBusesForStop(const Stop& stop);                                                        
+    
+        //поиск автобусов проходящих через остановку 
+        const std::unordered_set<const Bus*> FindBusesForStop(const std::string_view stop_name) const; 
+    
+        //получить информацию об остановке
+        const std::set<std::string_view> GetStopInfo(const std::string_view stop_name) const;          
+    
+        //задать дистанцию между остановками
+        void SetDistance(const Stop* from, const Stop* to, const int distance);                        
+    
+        //получить дистанцию между остановками
+        int GetDistance(const Stop* from, const Stop* to) const;                                       
     private:
-        std::deque<Stop> stops_;                                                                       //остановки
-        std::deque<Bus> buses_;                                                                        //маршруты
-        std::unordered_map<std::string_view, const Bus*> busname_to_bus_;                              //индекс остановок(хеш - таблица)
-        std::unordered_map<std::string_view, const Stop*> stopname_to_stop_;                           //индекс маршрутов(хеш - таблица)
-        std::unordered_map<const Stop*, std::unordered_set<const Bus*>> buses_for_stop_;               //автобусы проходящие через остановку
-        std::unordered_map<std::pair<const Stop*, const Stop*>, int, StopPairHasher> distances_;       //расстояние между остановками
+        std::deque<Stop> stops_;                                                                       
+        std::deque<Bus> buses_;                                                                        
+    
+        //индекс остановок(хеш - таблица)
+        std::unordered_map<std::string_view, const Bus*> busname_to_bus_;                              
+    
+        //индекс маршрутов(хеш - таблица)
+        std::unordered_map<std::string_view, const Stop*> stopname_to_stop_;                           
+        
+        //автобусы проходящие через остановку
+        std::unordered_map<const Stop*, std::unordered_set<const Bus*>> buses_for_stop_;               
+    
+        //расстояние между остановками
+        std::unordered_map<std::pair<const Stop*, const Stop*>, int, StopPairHasher> distances_;       
 };
 }
