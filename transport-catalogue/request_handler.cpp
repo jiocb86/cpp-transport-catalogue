8681379858f5773ba0b41a2dd14e20c8bc674c84
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-07-15 08:06:12 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,1 +1,21 @@
+#include "request_handler.h"
 
+bool RequestHandler::IsBusNumber(const std::string_view bus_number) const {
+    return catalogue_.FindBus(bus_number);
+}
+
+bool RequestHandler::IsStopName(const std::string_view stop_name) const {
+    return catalogue_.FindStop(stop_name);
+}
+
+catalogue::BusInfo RequestHandler::GetBusStat(const std::string_view& bus_number) const {
+    return catalogue_.GetBusInfo(bus_number);
+}
+
+const std::set<std::string_view> RequestHandler::GetBusesByStop(std::string_view stop_name) const {
+    return catalogue_.GetStopInfo(stop_name);
+}
+
+svg::Document RequestHandler::RenderMap() const {
+    return renderer_.GetSVG(catalogue_.GetSortedAllBuses());
+}
