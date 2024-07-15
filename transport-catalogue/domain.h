eac48862bf4c1b5e11a0b14a74294ced1fe2c3d7
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-07-15 08:00:57 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,1 +1,26 @@
+#pragma once
 
+#include <string>
+#include <vector>
+
+#include "geo.h"
+
+namespace catalogue {
+struct Stop {
+    std::string name;
+    geo::Coordinates coordinates;
+};
+
+struct Bus {
+    std::string number;
+    std::vector<const Stop*> route;
+    bool is_circle;
+};
+
+struct BusInfo {
+    int stops_count;
+    int unique_stops_count;
+    double geo_length;
+    int dist_length;
+};
+} // namespace catalogue
