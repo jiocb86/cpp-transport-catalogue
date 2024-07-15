652a926a8249a3b355d48426bb5aeb540808f308
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-07-15 08:05:55 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,1 +1,24 @@
+#pragma once
 
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+
+class RequestHandler {
+public:
+    explicit RequestHandler(const catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+        : catalogue_(catalogue)
+        , renderer_(renderer)
+    {
+    }
+
+    catalogue::BusInfo GetBusStat(const std::string_view& bus_number) const;
+    const std::set<std::string_view> GetBusesByStop(std::string_view stop_name) const;
+    bool IsBusNumber(const std::string_view bus_number) const;
+    bool IsStopName(const std::string_view stop_name) const;    
+    
+    svg::Document RenderMap() const;    
+
+private:
+    const catalogue::TransportCatalogue& catalogue_;
+    const renderer::MapRenderer& renderer_;    
+};
