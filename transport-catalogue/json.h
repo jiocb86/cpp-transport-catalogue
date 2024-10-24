6c9b481d613f69004261fdd21b1a8964c31298f0
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-10-25 01:39:51 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -26,21 +26,93 @@ public:
         
     Node(Value value) : variant(std::move(value)) {}        
         
-    const Array& AsArray() const;
-    const Dict& AsMap() const;
-    int AsInt() const;
-    double AsDouble() const;
-    bool AsBool() const;
-    const std::string& AsString() const;
+    const Array& AsArray() const {
+        using namespace std::literals;
+    
+        if (!IsArray()) {
+            throw std::logic_error("value is not an array"s);
+        }
+        return std::get<Array>(*this); 
+    }
  
-    bool IsNull() const;
-    bool IsInt() const;
-    bool IsDouble() const;
-    bool IsPureDouble() const;
-    bool IsBool() const;
-    bool IsString() const;
-    bool IsArray() const;
-    bool IsMap() const;
+    const Dict& AsDict() const {
+        using namespace std::literals;
+    
+        if (!IsDict()) {
+            throw std::logic_error("value is not a dictionary"s);
+        }
+        return std::get<Dict>(*this);  
+    }
+ 
+    const std::string& AsString() const {
+        using namespace std::literals;
+    
+        if (!IsString()) {
+            throw std::logic_error("value is not a string"s);
+        }
+        return std::get<std::string>(*this);        
+    }
+    
+    int AsInt() const {
+        using namespace std::literals; 
+    
+        if (!IsInt()) {
+            throw std::logic_error("value is not an int"s);
+        }
+        return std::get<int>(*this);    
+    }
+ 
+    double AsDouble() const {
+        using namespace std::literals;
+    
+        if (!IsDouble()) {
+            throw std::logic_error("value is not a double"s);
+        } else if (IsPureDouble()) {
+            return std::get<double>(*this);
+        }
+        return AsInt();
+    }
+ 
+    bool AsBool() const {
+        using namespace std::literals;
+    
+        if (!IsBool()) {
+            throw std::logic_error("value is not a bool"s);
+        }
+        return std::get<bool>(*this);   
+    }
+    
+    bool IsNull() const {
+        return std::holds_alternative<std::nullptr_t>(*this);
+    }    
+    
+    bool IsInt() const {
+        return std::holds_alternative<int>(*this);
+    }
+
+    bool IsDouble() const {
+        return IsPureDouble() || IsInt();
+    }
+
+    bool IsPureDouble() const {
+        return std::holds_alternative<double>(*this);
+    }    
+
+    bool IsBool() const {
+        return std::holds_alternative<bool>(*this);
+    }    
+
+    bool IsString() const {
+        return std::holds_alternative<std::string>(*this);
+    }    
+
+    bool IsArray() const {
+        return std::holds_alternative<Array>(*this);
+    }   
+    
+    bool IsDict() const {
+        return std::holds_alternative<Dict>(*this);
+    }
  
     const Value& GetValue() const {
         return *this;
