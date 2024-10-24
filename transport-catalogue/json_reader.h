b8c8e63d7d96a810ea6c250295c29fe2e3d3d987
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-10-25 01:41:05 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -51,20 +51,24 @@ public:
     const json::Node& GetBaseRequests() const;
     const json::Node& GetStatRequests() const;
     const json::Node& GetRenderSettings() const;    
+    const json::Node& GetRoutingSettings() const;    
     
     void ParseBaseRequests();
     void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;    
     void ApplyCommands(catalogue::TransportCatalogue& catalogue) const;
     svg::Color ParseColor(const json::Node& color_node) const;
     renderer::MapRenderer ParseRenderSettings(const json::Dict& request_map) const;
+    catalogue::Router FillRoutingSettings(const json::Node& settings) const;    
     
     const json::Node PrintRoute(const json::Dict& request_map, RequestHandler& rh) const;
     const json::Node PrintStop(const json::Dict& request_map, RequestHandler& rh) const;
     const json::Node PrintMap(const json::Dict& request_map, RequestHandler& rh) const;
+    const json::Node PrintRouting(const json::Dict& request_map, RequestHandler& rh) const;    
     
 private:
     json::Document input_;
     json::Node dummy_ = nullptr;
     std::vector<CommandDescription> commands_;
+    std::tuple<std::string_view, std::vector<const catalogue::Stop*>, bool> FillRoute(const json::Dict& request_map, catalogue::TransportCatalogue& catalogue) const;    
 };
 } // namespace reader
