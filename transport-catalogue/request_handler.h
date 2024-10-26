b5b1fe26c25560eb456228a37c792a5a8eb85e0b
jiocb86 <145716915+jiocb86@users.noreply.github.com>
2024-10-26 14:33:54 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -6,7 +6,8 @@
 
 class RequestHandler {
 public:
-    explicit RequestHandler(const catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const catalogue::Router& router)
+   
+    explicit RequestHandler(const catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const catalogue::TransportRouter& router)
         : catalogue_(catalogue)
         , renderer_(renderer)
         , router_(router)
@@ -18,12 +19,12 @@ public:
     bool IsBusNumber(const std::string_view bus_number) const;
     bool IsStopName(const std::string_view stop_name) const;    
     const std::optional<graph::Router<double>::RouteInfo> GetOptimalRoute(const std::string_view stop_from, const std::string_view stop_to) const;
-    const graph::DirectedWeightedGraph<double>& GetRouterGraph() const;    
+    const graph::DirectedWeightedGraph<double>* GetRouterGraph(const std::string_view stop_from, const std::string_view stop_to) const;    
     
     svg::Document RenderMap() const;    
 
 private:
     const catalogue::TransportCatalogue& catalogue_;
     const renderer::MapRenderer& renderer_;    
-    const catalogue::Router& router_;    
+    const catalogue::TransportRouter& router_;    
 };
